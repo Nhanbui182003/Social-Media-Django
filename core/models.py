6f608eeed828b67e33ff2cc71e2d86659b8dd4bf
Bui Nhan <nhantaygia182003@gmail.com>
2022-07-18 13:39:53 +0700

    Your message for the commit

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ USER = get_user_model()
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(USER, on_delete=models.CASCADE)
-    id_user = models.ImageField()
+    id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
--- core/views.py
+++ core/views.py
@@ -2,11 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+from .models import Profile
+
+from core.models import Profile
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -17,7 +22,18 @@ def signup(request):
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Taken')
-                return redirect('sginup')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'User name Taken')
+                return redirect('signup')
+            else :
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Password Not Maching')
             return redirect('signup')
